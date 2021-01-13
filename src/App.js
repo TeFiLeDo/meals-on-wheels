94dbd64defb8b130239f9d2dba92ff362b57866b
Adrian Wannenmacher <tfld@tfld.dev>
2021-01-13 21:28:08 +0100

    style(ui): :art: remove intentional malformatting

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import "semantic-ui-css/semantic.min.css";
 import SelectDataset from "./components/SelectDataset";
 
 export default class App extends React.Component {
-    state = {
-        state: "loading",
-    };
+  state = {
+    state: "loading",
+  };
 
-    selectDataset(year, month) {
-        alert(`Not yet implemented.\n\nYear:\t${year}\nMonth:\t${month}`);
-    }
+  selectDataset(year, month) {
+    alert(`Not yet implemented.\n\nYear:\t${year}\nMonth:\t${month}`);
+  }
 
-    componentDidMount() {
-        promisified({ cmd: "global", sub: { cmd: "getState" } })
-            .then((r) => {
-                if (r.variant !== "gotState") {
-                    throw new Error(
-                        `expected return variant 'gotState', got ${r.variant}`
-                    );
-                } else {
-                    this.setState({ state: r.state });
-                }
-            })
-            .catch((e) => console.log(e));
-    }
-
-    render() {
-        if (this.state.state === "select") {
-            return (
-                <SelectDataset
-                    selectDataset={this.selectDataset.bind(this)}
-                ></SelectDataset>
-            );
+  componentDidMount() {
+    promisified({ cmd: "global", sub: { cmd: "getState" } })
+      .then((r) => {
+        if (r.variant !== "gotState") {
+          throw new Error(
+            `expected return variant 'gotState', got ${r.variant}`
+          );
+        } else {
+          this.setState({ state: r.state });
         }
+      })
+      .catch((e) => console.log(e));
+  }
 
-        return <React.Fragment></React.Fragment>;
+  render() {
+    if (this.state.state === "select") {
+      return (
+        <SelectDataset
+          selectDataset={this.selectDataset.bind(this)}
+        ></SelectDataset>
+      );
     }
+
+    return <React.Fragment></React.Fragment>;
+  }
 }
