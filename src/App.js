37278b621b023ad78f52172db74b0a9256e78564
Adrian Wannenmacher <tfld@tfld.dev>
2021-01-13 21:18:53 +0100

    ci(ui): :art: intentionally malformated code
    This is to test the ci pipeline

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import "semantic-ui-css/semantic.min.css";
 import SelectDataset from "./components/SelectDataset";
 
 export default class App extends React.Component {
-  state = {
-    state: "loading",
-  };
+    state = {
+        state: "loading",
+    };
 
-  selectDataset(year, month) {
-    alert(`Not yet implemented.\n\nYear:\t${year}\nMonth:\t${month}`);
-  }
-
-  componentDidMount() {
-    promisified({ cmd: "global", sub: { cmd: "getState" } })
-      .then((r) => {
-        if (r.variant !== "gotState") {
-          throw new Error(
-            `expected return variant 'gotState', got ${r.variant}`
-          );
-        } else {
-          this.setState({ state: r.state });
-        }
-      })
-      .catch((e) => console.log(e));
-  }
-
-  render() {
-    if (this.state.state === "select") {
-      return (
-        <SelectDataset
-          selectDataset={this.selectDataset.bind(this)}
-        ></SelectDataset>
-      );
+    selectDataset(year, month) {
+        alert(`Not yet implemented.\n\nYear:\t${year}\nMonth:\t${month}`);
     }
 
-    return <React.Fragment></React.Fragment>;
-  }
+    componentDidMount() {
+        promisified({ cmd: "global", sub: { cmd: "getState" } })
+            .then((r) => {
+                if (r.variant !== "gotState") {
+                    throw new Error(
+                        `expected return variant 'gotState', got ${r.variant}`
+                    );
+                } else {
+                    this.setState({ state: r.state });
+                }
+            })
+            .catch((e) => console.log(e));
+    }
+
+    render() {
+        if (this.state.state === "select") {
+            return (
+                <SelectDataset
+                    selectDataset={this.selectDataset.bind(this)}
+                ></SelectDataset>
+            );
+        }
+
+        return <React.Fragment></React.Fragment>;
+    }
 }
