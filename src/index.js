0f23235ffff9facd8490a7a43705d872c03441f3
Adrian Wannenmacher <tfld@tfld.dev>
2021-01-20 18:43:57 +0100

    feat(ui): :globe_with_meridians: detect users language

--- src/index.js
+++ src/index.js
@@ -4,21 +4,34 @@ import App from "./App";
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import languageDetector from "i18next-browser-languagedetector";
 
 import de from "./locales/de";
 import en from "./locales/en";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    de,
-    en,
-  },
-  lng: "de",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(initReactI18next)
+  .use(languageDetector)
+  .init({
+    // localizations
+    resources: {
+      de,
+      en,
+    },
+
+    // language setup
+    detection: {
+      order: ["navigator"],
+    },
+    fallbackLng: "en",
+    supportedLngs: ["en", "de"],
+    nonExplicitSupportedLngs: true,
+
+    // other stuff
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 ReactDOM.render(
   <React.StrictMode>
