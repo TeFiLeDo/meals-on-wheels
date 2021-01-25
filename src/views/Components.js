84a105d820200aaa7b7d0d12256a6dfb8a44d478
Adrian Wannenmacher <tfld@tfld.dev>
2021-01-25 08:01:56 +0100

    feat(ui): :lipstick: turn component addition into modal

--- src/views/Components.js
+++ src/views/Components.js
@@ -10,6 +10,7 @@ import {
   Message,
   Input,
   Table,
+  Modal,
 } from "semantic-ui-react";
 import { promisified } from "tauri/api/tauri";
 import { handle_error, handle_unexpected_variant } from "../error";
@@ -18,6 +19,7 @@ export default function Components() {
   const { t } = useTranslation();
   const [newComponent, setNewComponent] = useState({
     name: "",
+    open: false,
     variants: [],
     variantsAvailable: [],
     options: [],
@@ -32,124 +34,20 @@ export default function Components() {
   return (
     <React.Fragment>
       <Header as="h2" attached="top">
-        <Icon name="plus" />{" "}
-        <Header.Content>{t("views.components.add.title")}</Header.Content>
-      </Header>
-      <Segment attached="bottom">
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-
-            promisified({
-              cmd: "component",
-              sub: {
-                cmd: "addComponent",
-                name: newComponent.name,
-                variants: newComponent.variants,
-                options: newComponent.options,
-              },
-            })
-              .then(() => {
-                setNewComponent((nc) => {
-                  return { ...nc, name: "", variants: [], options: [] };
-                });
-                alert("component added");
-              })
-              .catch((e) => handle_error(e, t));
-          }}
-        >
-          <Form.Field>
-            <label>{t("views.components.add.name_label")}</label>
-            <Input
-              placeholder={t("views.components.add.name_placeholder")}
-              value={newComponent.name}
-              onChange={(_, e) => {
-                setNewComponent((nc) => {
-                  return { ...nc, name: e.value };
-                });
-              }}
-            />
-            <Message
-              icon="info circle"
-              info
-              header={t("views.components.add.name_warning_title")}
-              content={t("views.components.add.name_warning_content")}
-            />
-          </Form.Field>
-
-          <Form.Group widths="equal">
-            <Form.Field
-              label={t("views.components.variants")}
-              placeholder={t("views.components.add.variants_placeholder")}
-              noResultsMessage={t("views.components.add.variants_none")}
-              additionLabel={t("views.components.add.variants_new")}
-              control={Select}
-              options={newComponent.variantsAvailable}
-              value={newComponent.variants}
-              onAddItem={(_, { value }) =>
-                setNewComponent((nc) => {
-                  return {
-                    ...nc,
-                    variantsAvailable: [
-                      { text: value, value },
-                      ...nc.variantsAvailable,
-                    ],
-                  };
-                })
-              }
-              onChange={(_, { value }) =>
-                setNewComponent((nc) => {
-                  return { ...nc, variants: value };
-                })
-              }
-              multiple
-              search
-              selection
-              allowAdditions
-            />
-            <Form.Field
-              label={t("views.components.options")}
-              placeholder={t("views.components.add.options_placeholder")}
-              noResultsMessage={t("views.components.add.options_none")}
-              additionLabel={t("views.components.add.options_new")}
-              control={Select}
-              options={newComponent.optionsAvailable}
-              value={newComponent.options}
-              onAddItem={(_, { value }) =>
-                setNewComponent((nc) => {
-                  return {
-                    ...nc,
-                    optionsAvailable: [
-                      { text: value, value },
-                      ...nc.optionsAvailable,
-                    ],
-                  };
-                })
-              }
-              onChange={(_, { value }) =>
-                setNewComponent((nc) => {
-                  return { ...nc, options: value };
-                })
-              }
-              multiple
-              search
-              selection
-              allowAdditions
-            />
-          </Form.Group>
-          <Button primary icon labelPosition="left" type="submit">
-            <Icon name="send" />
-            {t("views.components.add.add")}
-          </Button>
-        </Form>
-      </Segment>
-
-      <Header as="h2" attached="top">
         <Icon name="puzzle piece" />
         <Header.Content>{t("views.components.list.title")}</Header.Content>
       </Header>
       <Segment attached>
-        <Button primary disabled icon labelPosition="left">
+        <Button
+          primary
+          icon
+          labelPosition="left"
+          onClick={() =>
+            setNewComponent((nc) => {
+              return { ...nc, open: true };
+            })
+          }
+        >
           <Icon name="plus" />
           {t("views.components.list.add")}
         </Button>
@@ -157,10 +55,18 @@ export default function Components() {
       <Table striped attached="bottom">
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>{t("views.components.list.column_name")}</Table.HeaderCell>
-            <Table.HeaderCell>{t("views.components.list.column_variants")}</Table.HeaderCell>
-            <Table.HeaderCell>{t("views.components.list.column_options")}</Table.HeaderCell>
-            <Table.HeaderCell>{t("views.components.list.column_actions")}</Table.HeaderCell>
+            <Table.HeaderCell>
+              {t("views.components.list.column_name")}
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              {t("views.components.list.column_variants")}
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              {t("views.components.list.column_options")}
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              {t("views.components.list.column_actions")}
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -176,6 +82,148 @@ export default function Components() {
           })}
         </Table.Body>
       </Table>
+
+      <Modal open={newComponent.open}>
+        <Modal.Header content={t("views.components.add.title")} />
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <label>{t("views.components.add.name_label")}</label>
+              <Input
+                placeholder={t("views.components.add.name_placeholder")}
+                value={newComponent.name}
+                onChange={(_, e) => {
+                  setNewComponent((nc) => {
+                    return { ...nc, name: e.value };
+                  });
+                }}
+              />
+              <Message
+                icon="info circle"
+                info
+                header={t("views.components.add.name_warning_title")}
+                content={t("views.components.add.name_warning_content")}
+              />
+            </Form.Field>
+            <Form.Group widths="equal">
+              <Form.Field
+                label={t("views.components.variants")}
+                placeholder={t("views.components.add.variants_placeholder")}
+                noResultsMessage={t("views.components.add.variants_none")}
+                additionLabel={t("views.components.add.variants_new")}
+                control={Select}
+                options={newComponent.variantsAvailable}
+                value={newComponent.variants}
+                onAddItem={(_, { value }) =>
+                  setNewComponent((nc) => {
+                    return {
+                      ...nc,
+                      variantsAvailable: [
+                        { text: value, value },
+                        ...nc.variantsAvailable,
+                      ],
+                    };
+                  })
+                }
+                onChange={(_, { value }) =>
+                  setNewComponent((nc) => {
+                    return { ...nc, variants: value };
+                  })
+                }
+                multiple
+                search
+                selection
+                allowAdditions
+              />
+              <Form.Field
+                label={t("views.components.options")}
+                placeholder={t("views.components.add.options_placeholder")}
+                noResultsMessage={t("views.components.add.options_none")}
+                additionLabel={t("views.components.add.options_new")}
+                control={Select}
+                options={newComponent.optionsAvailable}
+                value={newComponent.options}
+                onAddItem={(_, { value }) =>
+                  setNewComponent((nc) => {
+                    return {
+                      ...nc,
+                      optionsAvailable: [
+                        { text: value, value },
+                        ...nc.optionsAvailable,
+                      ],
+                    };
+                  })
+                }
+                onChange={(_, { value }) =>
+                  setNewComponent((nc) => {
+                    return { ...nc, options: value };
+                  })
+                }
+                multiple
+                search
+                selection
+                allowAdditions
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            content={t("button.cancel")}
+            labelPosition="left"
+            icon="trash"
+            negative
+            onClick={() =>
+              setNewComponent((nc) => {
+                return {
+                  ...nc,
+                  name: "",
+                  open: false,
+                  variants: [],
+                  options: [],
+                };
+              })
+            }
+          />
+          <Button
+            content={t("views.components.add.add")}
+            labelPosition="left"
+            icon="plus"
+            positive
+            onClick={() => {
+              promisified({
+                cmd: "component",
+                sub: {
+                  cmd: "addComponent",
+                  name: newComponent.name,
+                  variants: newComponent.variants,
+                  options: newComponent.options,
+                },
+              })
+                .then((r) => {
+                  update(setComponents, t);
+                  return r;
+                })
+                .then((r) => {
+                  if (
+                    handle_unexpected_variant("addedComponent", r.variant, t)
+                  ) {
+                    setNewComponent((nc) => {
+                      return {
+                        ...nc,
+                        name: "",
+                        open: false,
+                        variants: [],
+                        options: [],
+                      };
+                    });
+                  }
+                })
+                .catch((e) => handle_error(e, t));
+            }}
+          />
+        </Modal.Actions>
+      </Modal>
     </React.Fragment>
   );
 }
